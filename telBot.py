1ddd380ffbd8de4d3f6200efdcf451388f2774d5
keiseto <keistonks@gmail.com>
2023-03-14 14:19:12 -0300

    q

--- telBot.py
+++ telBot.py
@@ -1,25 +1,35 @@
 import telegram
 from flask import Flask, request
 
-# Create a Flask app instance
 app = Flask(__name__)
+
+# Define a function to handle the /id command
+
+
+def get_user_id(update, context):
+    user_id = update.message.from_user.id
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Your user ID is: " + str(user_id))
+
 
 @app.route('/')
 def index():
     return 'Hello, world!'
 
-# Define a function to handle the /id command
-@app.route('/id', methods=['POST'])
-def get_user_id():
-    # Get the user ID from the request
-    user_id = request.json['message']['from']['id']
-    # Send a message back to the user with their ID
-    bot = telegram.Bot('6043717452:AAFkYEE7IMMuc_aYaTf9N3ZxXIcs4XLJrcQ')
-    webhook_url='https://python-projects-keiseto25.vercel.app//id'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # Dispatch the update to the handlers
+    dispatcher.process_update(update)
+    return 'ok'
+
+
+if __name__ == '__main__':
+    bot_token = '6043717452:AAFkYEE7IMMuc_aYaTf9N3ZxXIcs4XLJrcQ'
+    webhook_url = 'https://python-projects-git-main-keiseto25.vercel.app/webhook'
+    bot = telegram.Bot(bot_token)
     bot.setWebhook(url=webhook_url)
-    bot.send_message(chat_id=request.json['message']['chat']['id'], text="Your user ID is: " + str(user_id))
-    return ''
-
-if __name__ == '__main__':    
+    dispatcher = telegram.ext.Dispatcher(bot, None)
+    dispatcher.add_handler(telegram.ext.CommandHandler('id', get_user_id))
     app.run()
-
