9db4f5ae10f7726c0d4000dd3911434faad1cb67
keiseto <keistonks@gmail.com>
2023-03-14 14:43:15 -0300

    q

--- telBot.py
+++ telBot.py
@@ -1,36 +1,20 @@
-import telegram
-from flask import Flask, request
-
+from flask import Flask
+from flask import request
+from flask import Response
+import requests
+ 
 app = Flask(__name__)
-
-# Define a function to handle the /id command
-
-@app.route('/id', methods=['POST'])
-def get_user_id(update, context):
-    user_id = update.message.from_user.id
-    print("get user id function")
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Your user ID is: " + str(user_id))
-
-
-@app.route('/')
+ 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello, world!'
-
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    # Dispatch the update to the handlers
-    dispatcher.process_update(update)
-    return 'ok'
-
-
+    if request.method == 'POST':
+        msg = request.get_json()
+        print(msg)
+       
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
+ 
+ 
 if __name__ == '__main__':
-    bot_token = '6043717452:AAFkYEE7IMMuc_aYaTf9N3ZxXIcs4XLJrcQ'
-    webhook_url = 'https://python-projects-khaki.vercel.app'
-    bot = telegram.Bot(bot_token)
-    bot.setWebhook(url=webhook_url)
-    dispatcher = telegram.ext.Dispatcher(bot, None)
-    dispatcher.add_handler(telegram.ext.CommandHandler('id', get_user_id))
-    app.run()
+   app.run(debug=True)
