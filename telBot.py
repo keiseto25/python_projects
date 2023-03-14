68b149ab060eb586c2ababe0b8bdc6c3427e6663
keiseto <keistonks@gmail.com>
2023-03-14 14:09:23 -0300

    webhook

--- telBot.py
+++ telBot.py
@@ -11,9 +11,11 @@ def get_user_id():
     user_id = request.json['message']['from']['id']
     # Send a message back to the user with their ID
     bot = telegram.Bot('6043717452:AAFkYEE7IMMuc_aYaTf9N3ZxXIcs4XLJrcQ')
+    webhook_url='https://python-projects-khaki.vercel.app/id'
+    bot.setWebhook(url=webhook_url)
     bot.send_message(chat_id=request.json['message']['chat']['id'], text="Your user ID is: " + str(user_id))
     return ''
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     app.run()
 
