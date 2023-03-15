017299174ac352f9053d21f676a202bea1e1389c
keiseto <keistonks@gmail.com>
2023-03-15 18:54:24 -0300

    q

--- telBot.py
+++ telBot.py
@@ -56,6 +56,10 @@ def start(chat_id):
     }
     r = requests.post(url, json=payload)
     print("start-->", payload)
+    # sendMsg(chat_id,"Digite o valor mínimo da pool:")
+    # lowPrice = request.json['message']['text']
+    # sendMsg(chat_id,"Digite o valor máximo da pool:")
+    # highPrice = request.json['message']['text']
 
     return r.json()
 
@@ -65,77 +69,7 @@ def handle_callback(update):
     queryS = json.dumps(query)
     chat_id = query['message']['chat']['id']
     print(query)
-    pool_id = None
 
-    if pool_id is None:
-        pool_id = handleChoice(query, chat_id)
-    else:
-        # Send request to subgraph API
-        subgraph_url = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon'
-        query = """
-        {
-        pool(id:""" + pool_id + """){
-            token0 {
-            id
-            symbol
-            }
-            token0Price
-            token1 {
-            id
-            symbol
-            }
-            token1Price
-        }
-        }
-        """
-
-        response = requests.post(subgraph_url, headers={
-            'Content-Type': 'application/json'}, json={'query': query})
-        data = response.json()
-        pool = data['data']['pool']
-        token0, token0_price, token1, token1_price = pool['token0'], pool[
-            'token0Price'], pool['token1'], pool['token1Price']
-        tPrice = float(token1_price)
-        t0Symbol = token0['symbol']
-        t1Symbol = token1['symbol']
-        timestamp = datetime.now(pytz.timezone(
-            'America/Sao_Paulo')).strftime('%d/%m/%Y %H:%M:%S')
-
-        # Get user input for low_price and high_price
-        sendMsg(chat_id, 'Valor inicial:')
-        low_price_message = update['message']['text']
-        lowPrice = float(low_price_message.text)
-        sendMsg(chat_id, 'Valor final:')
-        high_price_message = update['message']['text']
-        highPrice = float(high_price_message.text)
-
-        pVariationlow = ((float(lowPrice) - tPrice) / float(lowPrice)) * 100
-        pVariationhigh = ((tPrice - float(highPrice)) / float(highPrice)) * 100
-
-        if (tPrice < lowPrice):
-            txt = '⚠️⬇️ <b>[' + timestamp + ']</b> : ' + t0Symbol + '/' + t1Symbol + ' abaixo de ' + \
-                str(lowPrice) + ': \n\n 👉 ' + str(tPrice) + \
-                '(-' + str(round(pVariationlow, 2)) + '%)'
-            print(txt)
-            sendMsg(chat_id, txt)
-            wLog(txt)
-        elif (tPrice > highPrice):
-            txt = '⚠️⬆️ <b>[' + timestamp + ']</b> : ' + t0Symbol + '/' + t1Symbol + ' acima de ' + \
-                str(highPrice) + ': \n\n 👉 ' + str(tPrice) + \
-                '(+' + str(round(pVariationhigh, 2)) + '%)'
-            print(txt)
-            sendMsg(chat_id, txt)
-            wLog(txt)
-        else:
-            txt = '[' + timestamp + '] : ' + t0Symbol + '/' + t1Symbol + ' dentro intervalo de ' + \
-                str(lowPrice) + ' a ' + str(highPrice) + \
-                ': \n\n ' + str(tPrice)
-            print(txt)
-            wLog(txt)
-        return response.json()
-
-
-def handleChoice(query, chat_id):
     choice = query['data']
     message_id = query['message']['message_id']
     # Perform action based on user choice
@@ -147,7 +81,6 @@ def handleChoice(query, chat_id):
         # Do something for MATIC/USDT pair
         pool_id = choice
         message = "You selected MATIC/USDT."
-    
 
     # Send message to user to confirm their choice
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
@@ -166,7 +99,70 @@ def handleChoice(query, chat_id):
         'reply_markup': {'inline_keyboard': []}
     }
     response = requests.post(url, json=payload)
-    return pool_id
+
+    # Send request to subgraph API
+    subgraph_url = 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon'
+    query = """
+        {
+        pool(id:""" + pool_id + """){
+            token0 {
+            id
+            symbol
+            }
+            token0Price
+            token1 {
+            id
+            symbol
+            }
+            token1Price
+        }
+        }
+        """
+
+    response = requests.post(subgraph_url, headers={
+        'Content-Type': 'application/json'}, json={'query': query})
+    data = response.json()
+    pool = data['data']['pool']
+    token0, token0_price, token1, token1_price = pool['token0'], pool[
+        'token0Price'], pool['token1'], pool['token1Price']
+    tPrice = float(token1_price)
+    t0Symbol = token0['symbol']
+    t1Symbol = token1['symbol']
+    timestamp = datetime.now(pytz.timezone(
+        'America/Sao_Paulo')).strftime('%d/%m/%Y %H:%M:%S')
+
+    # Get user input for low_price and high_price
+    sendMsg(chat_id, 'Valor inicial:')
+    low_price_message = update['message']['text']
+    lowPrice = float(low_price_message.text)
+    sendMsg(chat_id, 'Valor final:')
+    high_price_message = update['message']['text']
+    highPrice = float(high_price_message.text)
+
+    pVariationlow = ((float(lowPrice) - tPrice) / float(lowPrice)) * 100
+    pVariationhigh = ((tPrice - float(highPrice)) / float(highPrice)) * 100
+
+    if (tPrice < lowPrice):
+        txt = '⚠️⬇️ <b>[' + timestamp + ']</b> : ' + t0Symbol + '/' + t1Symbol + ' abaixo de ' + \
+            str(lowPrice) + ': \n\n 👉 ' + str(tPrice) + \
+            '(-' + str(round(pVariationlow, 2)) + '%)'
+        print(txt)
+        sendMsg(chat_id, txt)
+        wLog(txt)
+    elif (tPrice > highPrice):
+        txt = '⚠️⬆️ <b>[' + timestamp + ']</b> : ' + t0Symbol + '/' + t1Symbol + ' acima de ' + \
+            str(highPrice) + ': \n\n 👉 ' + str(tPrice) + \
+            '(+' + str(round(pVariationhigh, 2)) + '%)'
+        print(txt)
+        sendMsg(chat_id, txt)
+        wLog(txt)
+    else:
+        txt = '[' + timestamp + '] : ' + t0Symbol + '/' + t1Symbol + ' dentro intervalo de ' + \
+            str(lowPrice) + ' a ' + str(highPrice) + \
+            ': \n\n ' + str(tPrice)
+        print(txt)
+        wLog(txt)
+
 
 def wLog(message):
     with open('app.log', 'a') as f:
