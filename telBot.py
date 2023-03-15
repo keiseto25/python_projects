07adca34fbec6d16e3a394d7e2cf5eb3a7d63dc9
keiseto <keistonks@gmail.com>
2023-03-15 18:59:06 -0300

    q

--- telBot.py
+++ telBot.py
@@ -121,8 +121,8 @@ def handle_callback(update):
 
     response = requests.post(subgraph_url, headers={
         'Content-Type': 'application/json'}, json={'query': query})
-    print(response)
     data = response.json()
+    print(data)
     pool = data['data']['pool']
     token0, token0_price, token1, token1_price = pool['token0'], pool[
         'token0Price'], pool['token1'], pool['token1Price']
