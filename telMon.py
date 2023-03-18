3c86202ec1fb7ab99e3c97c8ea1a188bf260f543
keiseto <keistonks@gmail.com>
2023-03-18 01:37:37 -0300

    q

--- telMon.py
+++ telMon.py
@@ -288,7 +288,7 @@ def cronjob(data):
     else:
         print(f'Payload-->', job_data)
         print(f'Error updating job-->',response)
-    return response
+    return response.status_code
 
 
 def handle_input(chat_id, txt):
